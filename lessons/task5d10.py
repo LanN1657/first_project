14a7f36bd62e0dccd8da59e0fc8ec8c7335c7fef
SlncLanN <s1lenceav0ca@gmail.com>
2022-07-06 19:39:40 +0600

    Tasks

---
+++ lessons/task5d10.py
@@ -0,0 +1,9 @@
+try:
+    numbers = ['1', '2', '3']
+    a = numbers[0] * numbers[3]
+    print(numbrs)
+except (TypeError, IndexError, NameError):
+    print('error')
+else:
+    print('Correct!')
+    
